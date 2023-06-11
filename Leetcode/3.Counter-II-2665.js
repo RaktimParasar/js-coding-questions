ccb1020bb44176a6eace99bfda19b1ab30b7ce8d
Raktim Parasar <raktimparasar96@gmail.com>
2023-06-11 10:27:30 +0530

    leetcode day 3

---
+++ Leetcode/3.Counter-II-2665.js
@@ -0,0 +1,29 @@
+// Q.
+
+/**
+ * Write a function createCounter. It should accept an initial integer init.
+ * It should return an object with three functions.The three functions are:
+ * increment() increases the current value by 1 and then returns it.
+ * decrement() reduces the current value by 1 and then returns it.
+ * reset() sets the current value to init and then returns it.
+ */
+
+var createCounter = function (init) {
+  let currentValue = init;
+  return {
+    increment() {
+      return (currentValue += 1);
+    },
+    reset() {
+      return (currentValue = init);
+    },
+    decrement() {
+      return (currentValue -= 1);
+    },
+  };
+};
+
+const counter = createCounter(5);
+console.log(`Increment: ${counter.increment()}`);
+console.log(`Reset: ${counter.reset()}`);
+console.log(`Decrement: ${counter.decrement()}`);
