13d51754a09d1f938f17695a2e7b4d2c2d8fe958
Raktim Parasar <raktimparasar96@gmail.com>
2023-06-23 09:15:01 +0530

    leetcode-2621

---
+++ Leetcode/12.sleep-2621.js
@@ -0,0 +1,22 @@
+// Q.
+
+/**
+ * Given a positive integer millis, write an asynchronous function that sleeps
+ * for millis milliseconds. It can resolve any value.
+ */
+
+async function sleep(millis) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(millis);
+    }, millis);
+  });
+}
+
+async function sleep2(millis) {
+  await new Promise((resolve) => setTimeout(resolve, millis));
+}
+
+let t = Date.now();
+console.log(sleep(100).then(() => console.log(Date.now() - t)));
+console.log(sleep2(100).then(() => console.log(Date.now() - t)));
